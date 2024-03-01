560058d18012062ad636934c3fe186dce1d8e723
unknown <lukacc008@gmail.com>
2024-03-01 18:17:31 +0100

    Using ref to stop the timer even if more than one timer is started

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+
+//let timer; it doesnt work because if multiple challenges are started some pointers will be thrown away by react.
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
+  //let timer; cant be put here because of the state re-rendering, it has to be put outside the component function
+
   function handleStart() {
-    setTimerStarted(true);
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
+
+    setTimerStarted(true); //this will execute first even tho its not on top,because the timer will still run.
   }
 
-  function handleStop() {}
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
 
   return (
     <section className="challenge">
@@ -21,7 +30,7 @@ export default function TimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} Challange
         </button>
       </p>
