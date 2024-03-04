55116baa83346c6ee2ba69ea0f2e9da4f5c35481
unknown <lukacc008@gmail.com>
2024-03-04 14:14:00 +0100

    fixed it, the prop was wrong

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,9 +1,6 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
-  ref
-) {
+const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
 
   const dialog = useRef();
 
@@ -16,7 +13,7 @@ const ResultModal = forwardRef(function ResultModal(
   });
 
   return (
-    <dialog ref={ref} className="result-modal">
+    <dialog ref={dialog} className="result-modal">
       <h2>You {result}</h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
